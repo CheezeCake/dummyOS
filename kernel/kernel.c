9da40184e9144f4c4fa5ec4964b87d6e6c98124e
CheezeCake <emmanuel.nicolet@gmail.com>
2015-03-22 13:51:33 +0100

    remove puts

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,8 +5,6 @@ void kernel_main()
 	terminal_init();
 	for (int i = 0; i < 10; i++)
 		terminal_puts("Hello, world !\n");
-	for (int i = 0; i < 14; i++)
+	for (int i = 0; i < 15; i++)
 		terminal_puts("Ah ouais ?!\n");
-	terminal_puts("FUT !!!");
-	terminal_puts("\b\b");
 }
