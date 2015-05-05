42d2da83b55a9974eacb74143b5a1857a075816c
CheezeCake <emmanuel.nicolet@gmail.com>
2015-05-06 00:21:20 +0200

    Debug output for x86 on ioport 0xe9

---
+++ arch/x86/drivers/ioport_0xe9.c
@@ -0,0 +1,24 @@
+#include <kernel/libk.h>
+#include "ioport_0xe9.h"
+
+int ioport_0xe9_printf(const char* format, ...)
+{
+	const int buffer_size = 256;
+	char buffer[buffer_size];
+	va_list ap;
+
+	va_start(ap, format);
+	vsnprintf(buffer, buffer_size, format, ap);
+	va_end(ap);
+
+	return ioport_0xe9_puts(buffer);
+}
+
+int ioport_0xe9_puts(const char* str)
+{
+	int len;
+	for (len = 0; str[len]; len++)
+		ioport_0xe9_putchar(str[len]);
+
+	return len;
+}
---
+++ arch/x86/drivers/ioport_0xe9.h
@@ -0,0 +1,17 @@
+#ifndef _PORT_OXE9_
+#define _PORT_OXE9_
+
+#include "../io_ports.h"
+
+#define IOPORT_0XE9 0xe9
+
+int ioport_0xe9_printf(const char* format, ...)
+	__attribute__((format(printf, 1, 2)));
+int ioport_0xe9_puts(const char* str);
+
+static inline void ioport_0xe9_putchar(char c)
+{
+	outb(IOPORT_0XE9, c);
+}
+
+#endif
