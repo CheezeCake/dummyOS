d18a7e5af254ab808d224f19d1f15f46424980cc
CheezeCake <emmanuel.nicolet@gmail.com>
2015-06-24 21:18:00 +0200

    clean log.h inclusion

--- arch/x86/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LOG_H_
-#define _LOG_H_
-
-#include "drivers/ioport_0xe9.h"
-
-#define	log_printf(format, ...) ioport_0xe9_printf(format, __VA_ARGS__)
-#define	log_puts(str) ioport_0xe9_puts(str)
-
-#endif
