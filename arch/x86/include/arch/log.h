3d374acb6e30e785f89ce9ca28a2b100161a0a39
CheezeCake <emmanuel.nicolet@gmail.com>
2015-06-24 21:24:46 +0200

    fix log.h inclusion scheme

--- arch/x86/include/arch/log.h
+++ arch/x86/include/arch/log.h
@@ -1,7 +1,7 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
-#include "drivers/ioport_0xe9.h"
+#include "../../drivers/ioport_0xe9.h"
 
 #define	log_printf(format, ...) ioport_0xe9_printf(format, __VA_ARGS__)
 #define	log_puts(str) ioport_0xe9_puts(str)
--- include/kernel/log.h
+++ include/kernel/log.h
@@ -1,16 +1,1 @@
-#ifndef _KLOG_H_
-#define _KLOG_H_
-
-#ifdef ARCH_x86
-#include "../arch/x86/log.h"
-#endif
-
-#ifndef log_printf
-#define log_printf(format, ...)
-#endif
-
-#ifndef log_puts
-#define log_puts(str)
-#endif
-
-#endif
+#include <arch/log.h>
