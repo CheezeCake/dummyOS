867def2cf4791c1522247e6ac5beb61a0d34ce9e
CheezeCake <emmanuel.nicolet@gmail.com>
2015-09-02 09:58:27 +0200

    i8254 PIT

---
+++ arch/x86/i8254.c
@@ -0,0 +1,30 @@
+#include "i8254.h"
+#include "io_ports.h"
+
+int i8254_set_frequency(unsigned int frequency)
+{
+	// compute the value of the counter needed to get a
+	// interupt at approximately frequency Hz
+	unsigned int counter = I8254_FREQUENCY / frequency;
+
+	if (counter > I8254_MAX_FREQUENCY_DIVIDER)
+		return -1;
+
+	// for the chip 0 = I8254_MAX_FREQUENCY_DIVIDER,
+	// because I8254_MAX_FREQUENCY_DIVIDER cannot fit on 16 bits
+	// and you can't divide by 0
+	if (counter == I8254_MAX_FREQUENCY_DIVIDER)
+		counter = 0;
+
+	// Channel: 0
+	// Access mode: lobyte/hibyte
+	// Operating mode: rate generator
+	// http://wiki.osdev.org/Programmable_Interval_Timer#I.2FO_Ports
+	outb(I8254_MODE_COMMAND, 0x32);
+
+	// low 8 bits first
+	outb(I8254_CHANNEL0_PORT, counter & 0xff);
+	outb(I8254_CHANNEL0_PORT, (counter >> 8) & 0xff);
+
+	return 0;
+}
---
+++ arch/x86/i8254.h
@@ -0,0 +1,13 @@
+#ifndef _I8254_
+#define _I8254_
+
+#define I8254_FREQUENCY 1193182
+#define I8254_MAX_FREQUENCY_DIVIDER 65536
+
+#define I8254_CHANNEL0_PORT 0x40
+
+#define I8254_MODE_COMMAND 0x43
+
+int i8254_set_frequency(unsigned int frequency);
+
+#endif
