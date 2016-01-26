26cfe0138877dff4d13bcc6545fcaf700e88212d
CheezeCake <emmanuel.nicolet@gmail.com>
2016-01-26 20:36:58 +0100

    fix instruction names

--- arch/x86/io_ports.h
+++ arch/x86/io_ports.h
@@ -17,25 +17,25 @@ static inline uint8_t inb(uint16_t port)
 
 static inline void outw(uint16_t port, uint16_t value)
 {
-	__asm__ __volatile__ ("outb %w0, %w1" : : "a" (value), "d" (port));
+	__asm__ __volatile__ ("outw %w0, %w1" : : "a" (value), "d" (port));
 }
 
 static inline uint16_t inw(uint16_t port)
 {
 	uint16_t value;
-	__asm__ __volatile__ ("inb %w1, %w0" : "=a" (value) : "d" (port));
+	__asm__ __volatile__ ("inw %w1, %w0" : "=a" (value) : "d" (port));
 	return value;
 }
 
 static inline void outl(uint16_t port, uint32_t value)
 {
-	__asm__ __volatile__ ("outb %k0, %w1" : : "a" (value), "d" (port));
+	__asm__ __volatile__ ("outl %k0, %w1" : : "a" (value), "d" (port));
 }
 
 static inline uint32_t inl(uint16_t port)
 {
 	uint32_t value;
-	__asm__ __volatile__ ("inb %w1, %k0" : "=a" (value) : "d" (port));
+	__asm__ __volatile__ ("inl %w1, %k0" : "=a" (value) : "d" (port));
 	return value;
 }
 
