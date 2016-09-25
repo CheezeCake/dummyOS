29f874da41464f768265801c27495e42b90978e1
CheezeCake <emmanuel.nicolet@gmail.com>
2016-09-25 16:14:30 +0200

    debug_dump(): dump registers and print stacktrace

---
+++ arch/x86/debug.c
@@ -0,0 +1,65 @@
+#include <kernel/debug.h>
+#include <kernel/terminal.h>
+
+#define print_register(reg, value) \
+	terminal_printf("%s:\t0x%x\n", reg, value)
+
+static void debug_stacktrace(int frame, uint32_t* ebp)
+{
+	if (frame == 0)
+		return;
+
+	uint32_t* args = ebp + 2;
+	uint32_t eip = ebp[1];
+	if (eip == 0)
+		return;
+
+	ebp = (uint32_t*)ebp[0];
+
+	debug_stacktrace(frame - 1, ebp);
+
+	for (int i = 0; i < frame; i++)
+		terminal_putchar(' ');
+	terminal_printf("[0x%x] (0x%x, 0x%x, 0x%x)\n", (unsigned int)eip,
+			(unsigned int)args[0], (unsigned int)args[1],
+			(unsigned int)args[2]);
+}
+
+void debug_dump()
+{
+	uint32_t reg;
+
+#define REGISTERS(output) \
+	X(eax, output) \
+	X(ebx, output) \
+	X(ecx, output) \
+	X(edx, output) \
+	X(esi, output) \
+	X(edi, output) \
+	X(ebp, output) \
+	X(esp, output)
+
+	terminal_puts("\n");
+
+#define X(reg, output) { \
+	__asm__ ("movl %%"#reg", %0" : "=r" (output)); \
+	print_register(#reg, (unsigned int)output); \
+}
+	REGISTERS(reg)
+#undef X
+
+
+	__asm__ (
+			"pushfl\n"
+			"movl (%%esp), %0\n"
+			"addl $4, %%esp"
+			: "=r" (reg));
+	print_register("eflags", (unsigned int)reg);
+
+	uint32_t* ebp;
+	__asm__ ("movl %%ebp, %0" : "=r" (ebp));
+	ebp = (uint32_t*)ebp[0];
+
+	terminal_puts("\n=== STACKTRACE ===\n");
+	debug_stacktrace(DEBUG_FRAMES, ebp);
+}
---
+++ include/kernel/debug.h
@@ -0,0 +1,7 @@
+#ifndef _KDEBUG_H_
+#define _KDEBUG_H_
+
+#define DEBUG_FRAMES 5
+void debug_dump();
+
+#endif
