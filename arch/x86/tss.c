83c726033dfd13f280fe93a52cd364de54bf51fc
CheezeCake <emmanuel.nicolet@gmail.com>
2017-12-03 21:03:35 +0100

    [x86][tss] cleanup

--- arch/x86/tss.c
+++ arch/x86/tss.c
@@ -5,32 +5,6 @@
 
 static struct tss tss;
 
-static inline void tss_init_segment()
-{
-	const uint32_t base = (uint32_t)&tss;
-	const uint32_t limit = sizeof(struct tss);
-	gdt_init_system_segment(TSS, base, limit, PRIVILEGE_USER, TSS_32BIT);
-	/* struct gdt_segment_descriptor* tss_descr = &gdt[TSS]; */
-
-	/* tss_descr->limit_15_0 = limit & 0xffff; */
-
-	/* tss_descr->base_address_15_0 = base & 0xffff; */
-
-	/* tss_descr->base_23_16 = (base >> 16) & 0xff; */
-	/* tss_descr->type = TSS_32BIT; */
-	/* tss_descr->descriptor_type = 0; // 0 = system segment descriptor */
-	/* tss_descr->dpl = PRIVILEGE_USER; */
-	/* tss_descr->present = 1; */
-
-	/* tss_descr->limit_19_16 = (limit >> 16) & 0xf; */
-	/* tss_descr->avl = 0; */
-	/* tss_descr->zero = 0; */
-	/* tss_descr->operation_size = 1; */
-	/* tss_descr->granularity = 1; */
-
-	/* tss_descr->base_31_24 = base >> 24; */
-}
-
 void tss_update(uint32_t esp)
 {
 	tss.esp0 = esp;
@@ -41,7 +15,7 @@ void tss_init(void)
 	memset(&tss, 0, sizeof(struct tss));
 	tss.ss0 = make_segment_selector(PRIVILEGE_KERNEL, KDATA);
 
-	tss_init_segment();
+	gdt_init_system_segment(TSS, (uint32_t)&tss, sizeof(tss), PRIVILEGE_USER, TSS_32BIT);
 
 	// load TSS
 	__asm__ __volatile__ (
