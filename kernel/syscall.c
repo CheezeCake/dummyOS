6b46d7e7afb9f7a19229508b9b2c721458a20e42
CheezeCake <emmanuel.nicolet@gmail.com>
2017-12-20 00:56:21 +0100

    [syscalls] - check syscall number against SYSCALL_NR_TOP in asm syscall handler            - define syscall 0 (int nosys(void);)

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,3 +1,15 @@
-void* syscall_table[] = {
-	// [NR] = syscall,
+#include <stddef.h>
+
+#include <kernel/syscall.h>
+
+static int nosys(void);
+
+void* syscall_table[SYSCALL_NR_COUNT] = {
+	[NR_nosys] = nosys,
+	/* [NR_exit] = NULL, */
 };
+
+static int nosys(void)
+{
+	return 0;
+}
