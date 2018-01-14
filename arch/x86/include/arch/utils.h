b5d14dee7e2cf7b369cc65f18f360371c6a29a87
CheezeCake <emmanuel.nicolet@gmail.com>
2018-01-14 01:31:30 +0100

    inline le2h32(), le2h16()

--- arch/x86/include/arch/utils.h
+++ arch/x86/include/arch/utils.h
@@ -3,7 +3,12 @@
 
 #include <stdint.h>
 
-static uint32_t le2h32(uint32_t x)
+static inline uint32_t le2h32(uint32_t x)
+{
+	return x;
+}
+
+static inline uint16_t le2h16(uint16_t x)
 {
 	return x;
 }
