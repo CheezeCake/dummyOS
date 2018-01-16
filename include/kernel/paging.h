2cc2f25cfb6438304675d6a55d21f086dd9b65bb
CheezeCake <emmanuel.nicolet@gmail.com>
2018-01-16 22:06:35 +0100

    [paging] give VM_OPT_{READ,WRITE} the same value as ELF flags

--- include/kernel/paging.h
+++ include/kernel/paging.h
@@ -3,9 +3,11 @@
 
 #include <kernel/types.h>
 
-#define VM_OPT_READ 0
-#define VM_OPT_WRITE (1 << 0)
-#define VM_OPT_USER (1 << 1)
+// read and write: ELF values
+#define VM_OPT_WRITE	(1 << 1)
+#define VM_OPT_READ		(1 << 2)
+
+#define VM_OPT_USER		(1 << 3)
 
 void paging_init(void);
 int paging_map(p_addr_t paddr, v_addr_t vaddr, uint8_t flags);
