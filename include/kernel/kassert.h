2c6f72f3299525254c45c2aa298c3231657876d7
CheezeCake <emmanuel.nicolet@gmail.com>
2018-01-25 00:46:45 +0100

    kassert is only defined when NDEBUG is not

--- include/kernel/kassert.h
+++ include/kernel/kassert.h
@@ -3,12 +3,21 @@
 
 #include <kernel/panic.h>
 
+#ifdef NDEDUG
+
+#define kassert(expr) expr
+
+#else
+
 #define STR(x) STR1(x)
 #define STR1(x) #x
 
-#define kassert(expr) {\
-	if (expr) (void)0; \
-	else PANIC("Assertion failed: `" #expr "'"); \
-}
+#define kassert(expr)									\
+	do {												\
+		if (expr) (void)0;								\
+		else PANIC("Assertion failed: `" #expr "'");	\
+	} while (0)
+
+#endif
 
 #endif
