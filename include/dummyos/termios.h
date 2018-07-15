378d39f905f5ce6842d1a0816f1316b03fe98033
CheezeCake <emmanuel.nicolet@gmail.com>
2018-07-15 14:07:27 +0200

    move termios function prototypes to libc's <sys/termios.h>

--- include/dummyos/termios.h
+++ include/dummyos/termios.h
@@ -103,18 +103,4 @@ struct termios
 #define NOFLSH	(1 << 7) /* Disable flush after interrupt or quit. */
 #define TOSTOP	(1 << 8) /* Send SIGTTOU for background output. */
 
-speed_t cfgetispeed(const struct termios *);
-speed_t cfgetospeed(const struct termios *);
-int cfsetispeed(struct termios *, speed_t);
-int cfsetospeed(struct termios *, speed_t);
-int tcdrain(int);
-int tcflow(int, int);
-int tcflush(int, int);
-int tcgetattr(int, struct termios *);
-
-pid_t tcgetsid(int);
-
-int tcsendbreak(int, int);
-int tcsetattr(int, int, const struct termios *);
-
 #endif
